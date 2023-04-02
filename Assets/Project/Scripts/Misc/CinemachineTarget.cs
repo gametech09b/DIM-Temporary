d1d5c38f4b6e4bb895455ca6c5ed0668d219e7f4
Atqa Munzir <atqamz@gmail.com>
2023-04-02 13:28:23 +0700

    10.6_Cinemachine Target Group

--- Assets/Project/Scripts/Misc/CinemachineTarget.cs
+++ Assets/Project/Scripts/Misc/CinemachineTarget.cs
@@ -3,33 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-namespace DungeonGunner
-{
+namespace DungeonGunner {
     [RequireComponent(typeof(CinemachineTargetGroup))]
-    public class CinemachineTarget : MonoBehaviour
-    {
+    public class CinemachineTarget : MonoBehaviour {
         private CinemachineTargetGroup cinemachineTargetGroup;
 
+        [Tooltip("Cursor Target")]
+        [SerializeField] private Transform cursorTargetTransform;
 
-
-        private void Awake()
-        {
+        private void Awake() {
             cinemachineTargetGroup = GetComponent<CinemachineTargetGroup>();
         }
 
 
 
-        private void Start()
-        {
+        private void Start() {
             SetCinemachineTargetGroup();
         }
 
 
 
-        private void SetCinemachineTargetGroup()
-        {
+        private void Update() {
+            cursorTargetTransform.position = HelperUtilities.GetMouseWorldPosition();
+        }
+
+
+
+        private void SetCinemachineTargetGroup() {
             Transform currentPlayerTransform = GameManager.Instance.GetCurrentPlayer().transform;
-            cinemachineTargetGroup.AddMember(currentPlayerTransform, 1, 1);
+            cinemachineTargetGroup.AddMember(currentPlayerTransform, 1, 2.5f);
+
+            cinemachineTargetGroup.AddMember(cursorTargetTransform, 1, 1);
         }
     }
 }

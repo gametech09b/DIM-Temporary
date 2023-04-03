be819c8dd451fdacd9aa765f64809db877054802
Atqa Munzir <atqamz@gmail.com>
2023-04-04 03:23:20 +0700

    12.3_Object Pool Manager Implementation

---
+++ Assets/Project/Scripts/PoolManager/PoolManager.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonGunner {
+    [DisallowMultipleComponent]
+    public class PoolManager : SingletonMonobehaviour<PoolManager> {
+        [Tooltip("The pools to be created")]
+        [SerializeField] private Pool[] poolArray = null;
+
+        private Transform objectPoolTransform;
+        private Dictionary<int, Queue<Component>> poolDictionary = new Dictionary<int, Queue<Component>>();
+
+
+
+        private void Start() {
+            objectPoolTransform = transform;
+
+
+
+            foreach (Pool pool in poolArray) {
+                CreatePool(pool.prefab, pool.size, pool.componentType);
+            }
+        }
+
+
+
+        private void CreatePool(GameObject prefab, int size, string componentType) {
+            int key = prefab.GetInstanceID();
+
+            string poolName = $"{prefab.name} Pool";
+
+            GameObject parentPoolGameObject = new GameObject(poolName);
+            parentPoolGameObject.transform.SetParent(objectPoolTransform);
+
+            if (!poolDictionary.ContainsKey(key)) {
+                poolDictionary.Add(key, new Queue<Component>());
+
+                for (int i = 0; i < size; i++) {
+                    GameObject newPoolGameObject = Instantiate(prefab, parentPoolGameObject.transform);
+                    newPoolGameObject.SetActive(false);
+
+                    if (componentType != "") {
+                        newPoolGameObject.AddComponent(System.Type.GetType(componentType));
+                    }
+
+                    poolDictionary[key].Enqueue(newPoolGameObject.GetComponent(Type.GetType(componentType)));
+                }
+            }
+        }
+
+
+
+        public Component ReuseComponent(GameObject prefab, Vector3 position, Quaternion rotation) {
+            int key = prefab.GetInstanceID();
+
+            if (poolDictionary.ContainsKey(key)) {
+                Component componentToReuse = GetComponentFromPool(key);
+
+                ResetObject(componentToReuse, prefab, position, rotation);
+
+                return componentToReuse;
+            }
+
+            Debug.LogWarning($"Pool with key {key} doesn't exist.");
+            return null;
+        }
+
+
+
+        private Component GetComponentFromPool(int key) {
+            Component componentToReuse = poolDictionary[key].Dequeue();
+
+            poolDictionary[key].Enqueue(componentToReuse);
+
+            if (componentToReuse.gameObject.activeSelf) {
+                componentToReuse.gameObject.SetActive(false);
+            }
+
+            return componentToReuse;
+        }
+
+
+
+        private void ResetObject(Component componentToReuse, GameObject prefab, Vector3 position, Quaternion rotation) {
+            componentToReuse.transform.position = position;
+            componentToReuse.transform.rotation = rotation;
+            componentToReuse.transform.localScale = prefab.transform.localScale;
+        }
+
+
+
+        #region Validation
+#if UNITY_EDITOR
+        private void OnValidate() {
+            HelperUtilities.ValidateCheckEnumerableValues(this, nameof(poolArray), poolArray);
+        }
+#endif
+        #endregion
+    }
+
+
+
+    [System.Serializable]
+    public struct Pool {
+        public GameObject prefab;
+        public int size;
+        public string componentType;
+    }
+}
